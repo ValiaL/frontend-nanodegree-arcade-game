5d184d3455201ad4a7c1240a23b8bf7eb9cef32b
ValiaL <wahia83@gmail.com>
2014-12-24 14:20:13 +0200

    Display maze and add bugs

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+//Array of random speeds for the bugs
+var BUG_SPEEDS = [150,200,250,300,350,400,450,500,550];
+var BUG_START_Y = [145,230,315,395];
+var BUG_START_X = 100;
+
+
+//Produce a random number
+var randomNumber = function(range) {
+return Math.floor(Math.random()*range);
+};
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
@@ -6,7 +21,21 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = BUG_START_X;
+    this.y = this.StartPosY();
+    this.speed = 17;
 }
+
+/**
+* Computes the random y position of bug entities;
+* range of start position determined by BUG_START_Y
+*/
+Enemy.prototype.StartPosY = function() {
+    console.log(BUG_START_Y[randomNumber(BUG_START_Y.length)]);
+    return BUG_START_Y[randomNumber(BUG_START_Y.length)];
+}
+
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -14,6 +43,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +55,39 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+//Main player
+var Player = function() {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-horn-girl.png';
+    this.x = 100;
+    this.y = 400;
+}
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var enemy1 = new Enemy();
+var enemy2 = new Enemy();
+var enemy3 = new Enemy();
+var enemy4 = new Enemy();
+var player = new Player();
 
-
+var allEnemies = [enemy1,enemy2,enemy3,enemy4];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -41,6 +98,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
